58e058abe2581048a52b46218d3e1e15c3e4c190
Josh Nussbaum <joshnuss@gmail.com>
2024-11-16 22:44:30 -0500

    Adds prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 // @ts-check
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
+  prettierRecommended,
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
